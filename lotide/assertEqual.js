d0fbe699f062b81119b2781806ae550e88a6f486
Calvin Ho <calvin.ch.ho@gmail.com>
2022-05-31 01:25:34 -0400

    Writing our own colourful assert function

---
+++ lotide/assertEqual.js
@@ -0,0 +1,14 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`👍✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return (`👎❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//TEST CODE
+console.log(assertEqual("Lighthouse Labs", "Lighthouse Labs"));
+console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
+console.log(assertEqual(1, 1));
+console.log(assertEqual(1, 1.1));
