e842799e9553070fda2558a5850a39224f5ae8e2
Calvin Ho <calvin.ch.ho@gmail.com>
2022-05-31 02:12:56 -0400

    Using assertEqual to compare the tail of an array

---
+++ lotide/tail.js
@@ -0,0 +1,34 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`👍✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return (`👎❌Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let arrayTail = array.slice(1, array.length);
+  //if (array.length = 1)
+  return arrayTail;
+};
+
+// Test Case 1: Check the returned array
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(assertEqual(result.length, 2));
+console.log(assertEqual(result[0], "Lighthouse"));
+console.log(assertEqual(result[1], "Labs"));
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+console.log(assertEqual(words.length, 3));
+
+// Test Case: An array with one element
+const names = ["Calvin"];
+console.log(assertEqual(tail(names).length, 0));
+
+// Test Case: An array with one element
+const empty = [];
+console.log(tail(empty));
+console.log(assertEqual(tail(empty).length, 0));
